70ed27acde4751bad53033ce2e52740d891557e3
Harry Rickards <hrickards@gmail.com>
2014-03-22 10:21:02 +0000

    Public wifi doesn't like port 57107

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh deployer@vps "cd apps/social_impact;git pull;./kill.sh;bundle exec unicorn -c config/unicorn.rb -D"
+ssh deployer@harryrickards.com "cd apps/social_impact;git pull;./kill.sh;bundle exec unicorn -c config/unicorn.rb -D"
